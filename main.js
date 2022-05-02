7d41408b14fe35cb43bb9fa0891fee4ebe190e1a
Luis Felipe <luis.felipe.almeida.nascimento@gmail.com>
2022-05-02 15:47:37 -0300

    episode 14

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
-window.Event = new Vue();
 
-Vue.component('coupon',{
-    template:'<input placeholder="Code here" @blur="onCouponApplied">',
-    methods:{
-        onCouponApplied: function(){
-            Event.$emit('applied');
-        }
-    }
+Vue.component('modal',{
+    template:
+   `<div class="modal is-active">
+        <div class="modal-background"></div>
+        <div class="modal-card">
+        <header class="modal-card-head">
+            <p class="modal-card-title">
+                <slot name="header"><slot>
+            </p>
+            <button class="delete" aria-label="close"></button>
+        </header>
+        <section class="modal-card-body">
+            <slot></slot>
+        </section>
+        <footer class="modal-card-foot">
+            <slot name="footer"></slot>
+            <button class="button">OK</button>
+        </footer>
+        </div>
+    </div>`,
 })
 
 const app = new Vue({
     el:'#root',
     data: {
-        couponApplied:false
-    },
-    
-    created(){
-        Event.$on('applied',()=>alert('Handling it'))
     }
+    
+    
 })
