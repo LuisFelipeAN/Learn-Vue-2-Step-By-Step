a2296d2724d1bf36ace986062ea56b94f97d2a65
Luis Felipe <luis.felipe.almeida.nascimento@gmail.com>
2022-05-02 12:03:53 -0300

    episode 8

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+Vue.component('task-list',{
+    template:'<div><task v-for="task in tasks">{{task.description}}</task></div>',
+
+    data(){
+        return{
+            tasks: [
+                {description: 'Task 1', completed: true},
+                {description: 'Task 2', completed: false},
+                {description: 'Task 3', completed: true},
+                {description: 'Task 4', completed: false},
+                {description: 'Task 5', completed: true},
+            ]
+        }
+    }
+
+})
 Vue.component('task',{
     template:'<li><slot></slot></li>'
 })
